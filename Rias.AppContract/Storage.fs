1b3419e323a2b094266cb1407506e2143a7508c6
Przemysław Sowiński <psowinski@users.noreply.github.com>
2018-10-19 20:15:38 +0200

    Storage contract

---
+++ Rias.AppContract/Storage.fs
@@ -0,0 +1,9 @@
+﻿namespace Rias.AppContract
+
+[<AutoOpen>]
+module StorageContract =
+
+    type Storage<'event> = {
+        Store: 'event seq -> Result<unit, string>
+        Load: string -> Result<'event seq, string>
+    }
