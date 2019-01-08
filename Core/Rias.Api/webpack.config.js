15e7dc06978a516748e2aad9c0235828759f5df7
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2019-01-08 19:33:00 +0100

    Compile with fable

---
+++ Core/Rias.Api/webpack.config.js
@@ -0,0 +1,57 @@
+var path = require("path");
+var webpack = require("webpack");
+
+function resolve(filePath) {
+  return path.join(__dirname, filePath)
+}
+
+var babelOptions = {
+  presets: ["@babel/env"],
+  plugins: ["@babel/plugin-transform-modules-commonjs"]
+};
+
+var isProduction = process.argv.indexOf("-p") >= 0;
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: "source-map",
+  entry: resolve('./project.fsproj'),
+  output: {
+    filename: 'bundle.js',
+    path: resolve('./dist'),
+    library: "lib",
+    libraryTarget: "commonjs2"
+  },
+  resolve: {
+    modules: [
+      "node_modules", resolve("./node_modules/")
+    ]
+  },
+  devServer: {
+    contentBase: resolve('./public'), //http://localhost:8085/
+    port: 8085
+  },
+  module: {
+    rules: [
+      {
+        test: /\.fs(x|proj)?$/,
+        use: {
+          loader: "fable-loader",
+          options: {
+            babel: babelOptions,
+            define: isProduction ? [] : ["DEBUG"]
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      }
+    ]
+  }
+}
